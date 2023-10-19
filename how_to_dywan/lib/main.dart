7708bad2387cec3d1be9b4d760afefad8f625354
Xyriachi <116439184+Xyriachi@users.noreply.github.com>
2023-10-19 19:07:47 +0200

    App bars
    Top appbar with app name and settings icon,
    Bottom navigation bar with 4 icons of main categories

--- how_to_dywan/lib/main.dart
+++ how_to_dywan/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool isScrolled) {
+            return [
+              //TODO change color of appbar when visible
+              SliverAppBar(
+                title: const Text('How to Dywan?'),
+                actions: [
+                  PopupMenuButton(itemBuilder: (BuildContext context) {
+                    return const [
+                      PopupMenuItem(child: Text('TODO')),
+                      PopupMenuItem(child: Text('TODO')),
+                      PopupMenuItem(child: Text('TODO')),
+                    ];
+                  })
+                ],
+                floating: true,
+                snap: true,
+              )
+            ];
+          },
+          body: const Text('Hello!'),
         ),
+        bottomNavigationBar: NavigationBar(
+            onDestinationSelected: (value) => {print(value)},
+            destinations: const [
+              //TODO bloc state for bottom navigation bar
+              NavigationDestination(
+                icon: Icon(Icons.library_books_outlined),
+                selectedIcon: Icon(Icons.library_books),
+                label: 'Basic',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.school_outlined),
+                selectedIcon: Icon(Icons.school),
+                label: 'Advanced',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.menu_book_rounded),
+                label: 'Tactics',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.extension_outlined),
+                selectedIcon: Icon(Icons.extension),
+                label: 'Addons',
+              ),
+            ]),
       ),
     );
   }
